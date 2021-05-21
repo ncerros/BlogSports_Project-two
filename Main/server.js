44f688fbf92a8d983e5ee78218a5a374690dca0e
Ali Arab <aliarab812@yahoo.com>
2021-05-21 13:26:27 -0500

    name added to profile

--- Main/server.js
+++ Main/server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Sport Blog is LIVE on http://localhost:${PORT}`));
 });
 
