189fed66635b1973078091b568332d8b9333c146
Norman Cerros <n_cerros@hotmail.com>
2021-06-08 10:12:15 -0600

    modified and created files

---
+++ publicblog/js/main.js
@@ -0,0 +1,90 @@
+const blogForm = document.getElementById('blog-form');
+const blogMessages = document.querySelector('.blog-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// will find username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Joining  blogroom
+socket.emit('joinRoom', { username, room });
+
+// Go to room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// will  Message from server
+socket.on('message', (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scrolling down
+  blogMessages.scrollTop = blogMessages.scrollHeight;
+});
+
+// This will submit the message
+blogForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // This will get message text
+  let msg = e.target.elements.msg.value;
+
+  msg = msg.trim();
+
+  if (!msg) {
+    return false;
+  }
+
+  // This will sent message to server
+  socket.emit('blogMessage', msg);
+
+  // This will Clear all the input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// This action will message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.blog-messages').appendChild(div);
+}
+
+// Created room name will be added to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Created users will be added to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//This will prompt the user before leaving blog room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('You are leaving the blogroom!');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// const session = require('express-session');
+// const routes = require('./routes');
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const http = require('http');
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const socketio =require('socket.io');
+
+const server = http.createServer(app);
 const app = express();
+const io = cocketio(server);
+
+
+
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({  });
+
 const PORT = process.env.PORT || 3001;
-
-
 
 // Configure and link a session object with the sequelize store
 const sess = {
@@ -30,13 +40,24 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-// app.get("/", requestNews);
-// turn on routes
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'publicblog')));
+
+// Run when the user connetcs
+io.on('connection', socket => {
+  console.log('New WS Connection');
+
+  socket.emit('message', 'Welcome to BlogSports');
+
+  // broadcating when users connect
+  socket.broadcast.emit('message', 'A user has joined the blog');
+
+  
+
+});
+// app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Sport Blog is LIVE on http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Blog Sports  is LIVE on http://localhost:${PORT}`));
 });
 
